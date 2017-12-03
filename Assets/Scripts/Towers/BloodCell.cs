e787793f1602eb6651333ba75c816b6131c26897
Jordan Kniest <mail@jkniest.de>
2017-12-03 22:50:54 +0100

    Blood cells are now shooting bullets

--- Assets/Scripts/Towers/BloodCell.cs
+++ Assets/Scripts/Towers/BloodCell.cs
@@ -21,40 +21,40 @@ namespace LD40.Towers
                 enemy = GetNearestEnemy();
             }
 
-            if (enemy != null)
+            if (enemy == null) return;
+
+            if (Vector3.Distance(enemy.transform.position, transform.position) > Radius)
             {
-                if (Vector3.Distance(enemy.transform.position, transform.position) > Radius)
-                {
-                    enemy = null;
-                    return;
-                }
-                
-                var lookPos = enemy.transform.position - Head.position;
-                lookPos.y = 0;
-                var rotation = Quaternion.LookRotation(lookPos);
-                rotation = Quaternion.Euler(-90, rotation.eulerAngles.y, rotation.eulerAngles.z + 90);
-                Head.rotation = Quaternion.Slerp(Head.rotation, rotation, Time.deltaTime * 10f);
-
-                timer -= Time.deltaTime;
-                if (timer > 0) return;
-
-                GetComponent<Animator>().SetBool("shooting", true);
-                StartCoroutine("DisableShooting");
-                
-                timer = Delay;
+                enemy = null;
+                return;
             }
+
+            var lookPos = enemy.transform.position - Head.position;
+            lookPos.y = 0;
+            var rotation = Quaternion.LookRotation(lookPos);
+            rotation = Quaternion.Euler(-90, rotation.eulerAngles.y, rotation.eulerAngles.z + 90);
+            Head.rotation = Quaternion.Slerp(Head.rotation, rotation, Time.deltaTime * 20f);
+
+            timer -= Time.deltaTime;
+            if (timer > 0) return;
+
+            GetComponent<Animator>().SetBool("shooting", true);
+            StartCoroutine("DisableShooting");
+
+            timer = Delay;
         }
 
         private Collider GetNearestEnemy()
         {
             return Physics.OverlapSphere(transform.position, Radius)
-                .FirstOrDefault(enemy => enemy.GetComponent<Enemy>() != null);
+                .FirstOrDefault(enemy =>
+                    enemy.GetComponent<Enemy>() != null && enemy.GetComponent<Enemy>().IsPulled() == false);
         }
 
         private IEnumerator DisableShooting()
         {
             yield return new WaitForEndOfFrame();
-            
+
             GetComponent<Animator>().SetBool("shooting", false);
         }
 
@@ -63,6 +63,7 @@ namespace LD40.Towers
             var bullet = Instantiate(BulletPrefab);
             bullet.transform.position = ShotSpawn.position;
             bullet.transform.rotation = Head.transform.rotation;
+            bullet.GetComponent<Bullet>().SetDamage(Damage);
         }
     }
 }
